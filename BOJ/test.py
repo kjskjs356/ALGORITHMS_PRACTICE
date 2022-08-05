ed8a5f8c5a454dc5d40e3219e2a6740f1853e596
kjskjs356 <kjskjs356@gmail.com>
2022-08-05 23:19:18 +0900

    2022-08-05

--- BOJ/test.py
+++ BOJ/test.py
@@ -1,10 +1,37 @@
-# def func_a(a, b= 3, c=7, *args, **kwargs):
-#   d = [n*2 for n in args if n >2]
-#   e = [n**2 for n in kwargs]
-#   Sum = a + b + c + d + e
-#   return Sum
-# print(func_a(1,3,6,1,3,7,a=3,b=4))
+def solution(servers, sticky, requests):
+    answer = [[] for _ in range(servers)]
+    cnt = 0
+    num = 0
+    while cnt != len(requests):
+        if not sticky:
+            answer[num].append(requests[cnt])
+            num += 1
+            cnt += 1
+            if num == len(answer):
+                num = 0
+        else:
+            if cnt == 0:
+                answer[num].append(requests[cnt])
+                cnt += 1
+                continue
 
-# print(True + False)
-a= 'python'
-print(a[3:8])
+            if requests[cnt] == requests[cnt - 1]:
+                answer[num].append(requests[cnt])
+                cnt += 1
+
+            else:
+                if num == len(answer) - 1:
+                    num = 0
+                else:
+                    num += 1
+                answer[num].append(requests[cnt])
+                cnt += 1
+    while [] in answer:
+        answer.remove([])
+    return answer
+
+servers = 4
+sticky = True
+requests = [1]
+
+print(solution(servers, sticky, requests))
