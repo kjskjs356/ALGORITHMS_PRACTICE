435fa75eb81e94dc1d6dad059e4f2442e0a9ca3a
kjskjs356 <kjskjs356@gmail.com>
2022-09-04 20:17:48 +0900

    2022-09-04

--- BOJ/boj18310.py
+++ BOJ/boj18310.py
@@ -3,7 +3,6 @@
 N = int(input())
 locate = list(map(int, input().split()))
 locate.sort()
-result = 0
 # 가장 가운데에 위치한 건물에 안테를 지어야 거리값이 최소가 되므로
 # 간단하게 건물의 위치중 중간에 위치한 건물을 출력
 print(locate[(N - 1) // 2])
