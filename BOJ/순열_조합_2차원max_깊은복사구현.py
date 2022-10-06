0880c92ffbe5b35eafd11bc86cfcdbcb3496fe09
kjskjs356 <kjskjs356@gmail.com>
2022-10-06 17:32:13 +0900

    2022-10-06

--- BOJ/순열_조합_2차원max_깊은복사구현.py
+++ BOJ/순열_조합_2차원max_깊은복사구현.py
@@ -30,6 +30,36 @@ def comb(arr, n):
     return result
 
 
+# 배열 90도 회전
+def rotate_90(arr):
+    N = len(arr)
+    ret = [[0] * N for _ in range(N)]
+    for r in range(N):
+        for c in range(N):
+            ret[c][N-1-r] = arr[r][c]
+    return ret
+
+
+# 배열 180도 회전
+def rotate_180(arr):
+    N = len(arr)
+    ret = [[0] * N for _ in range(N)]
+    for r in range(N):
+        for c in range(N):
+            ret[N-1-r][N-1-c] = arr[r][c]
+    return ret
+
+
+# 배열 -90도 회전
+def rotate_270(arr):
+    N = len(arr)
+    ret = [[0] * N for _ in range(N)]
+    for r in range(N):
+        for c in range(N):
+            ret[N-1-c][r] = arr[r][c]
+    return ret
+
+
 area = [[1, 2, 3, 4], [5, 6, 7, 8]]
 
 # 2차원 배열 깊은 복사
