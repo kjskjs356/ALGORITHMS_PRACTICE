7313e550ee9247e33b73976db58430c618cb71da
JS <72597437+kjskjs356@users.noreply.github.com>
2023-03-07 11:40:49 +0900

    [Bronze III] Title: 직각삼각형, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,11 @@
+# 4153 직각삼각형
+
+while True:
+    arr = list(map(int, input().split()))
+    if sum(arr) == 0:
+        break
+    arr.sort()
+    if arr[0]**2 + arr[1]**2 == arr[2]**2:
+        print('right')
+    else:
+        print('wrong')
