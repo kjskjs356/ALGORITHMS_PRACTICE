896463628310fb1648c7be703068f5ebf0bd882d
JS <72597437+kjskjs356@users.noreply.github.com>
2023-03-17 14:11:03 +0900

    [Gold III] Title: 선분 덮기, Time: 448 ms, Memory: 126580 KB -BaekjoonHub

---
+++ 백준/Gold/2024. 선분 덮기/선분 덮기.py
@@ -0,0 +1,38 @@
+# 2024 선분덮기
+
+from collections import deque
+
+# 0부터 M 까지의 범위를 선으로 덮기
+M = int(input())
+line = []
+total = [0] * (M + 1)
+ans = 0
+# 선 정보 입력(0, 0) 나오면 종료
+while True:
+    L, R = map(int, input().split())
+    if L > R: L, R = R, L
+    if L == 0 and R == 0: break
+    if L == R or L > M or R < 0: continue
+    line.append((L, R))
+line.sort(key=lambda x:(x[0], x[1]))
+cur = 0
+idx = 0
+while True:
+    end = -1
+    # 선분의 시작점이 전체 시작점 이하일 경우
+    while idx < len(line):
+        if line[idx][0] <= cur:
+            if line[idx][1] > end:
+                end = line[idx][1]
+            else:
+                idx += 1
+        else:
+            break
+    if end == -1:
+        print(0)
+        break
+    ans += 1
+    if end >= M:
+        print(ans)
+        break
+    cur = end
