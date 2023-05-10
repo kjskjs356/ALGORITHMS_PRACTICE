2e5630cc67b2812db3cd17e2c43f4a0272b9d1ce
JS <72597437+kjskjs356@users.noreply.github.com>
2023-05-10 09:07:52 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+#define endl '\n'
+
+int N, num;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> N;
+	while (N--) {
+		// 0이 입력되면 큰 수 출력 or 비어있으면 0 출력
+		cin >> num;
+		if (num == 0) if (pq.empty()) cout << 0 << endl;
+		else {
+			cout << pq.top() << endl;
+			pq.pop();
+		}
+		else pq.push(num);
+	}
+
+	return 0;
+}
